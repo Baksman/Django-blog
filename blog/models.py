6b9fe4e47df92165cc79093baab734a487de3c74
Baksman <Ibrahimshehu73700@gmail.com>
2020-08-08 16:28:00 +0100

    added delete view

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,8 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post-detail",kwargs={"pk":self.pk})
 
+
+
     
 
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
@@ -52,6 +52,18 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    
 
 
 @login_required
